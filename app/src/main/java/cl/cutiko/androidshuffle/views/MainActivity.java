a44e2ffd8eb58bf23e417706f9b537ec3cac90ad
cutiko <cutiko@gmail.com>
2016-07-15 12:20:20 -0400

    REMOVE: on post create override not needed

--- app/src/main/java/cl/cutiko/androidshuffle/views/MainActivity.java
+++ app/src/main/java/cl/cutiko/androidshuffle/views/MainActivity.java
@@ -11,11 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
         getSupportActionBar().hide();
     }
+
+
 }
