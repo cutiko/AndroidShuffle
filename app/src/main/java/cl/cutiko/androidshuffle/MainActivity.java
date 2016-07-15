0ddd40c6af0dbd2f356ff7176bd66a47d36c21e0
cutiko <cutiko@gmail.com>
2016-07-15 12:02:38 -0400

    ADD: first commit. ADD: style working

---
+++ app/src/main/java/cl/cutiko/androidshuffle/MainActivity.java
@@ -0,0 +1,19 @@
+package cl.cutiko.androidshuffle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getSupportActionBar().hide();
+    }
+}
